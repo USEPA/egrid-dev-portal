d2a9a6774059d5fe05b53aab33f01ee695705f08
Kevin Hollerbach <58224863+kevinhollerbach@users.noreply.github.com>
2025-06-10 10:49:09 -0400

    Update and rename main-n5cmIS1M.js to main-gPOf-SiZ.js

---
+++ assets/main-gPOf-SiZ.js
@@ -0,0 +1,65 @@
+---
+title: "eGRID Dev Portal"
+introTitle: "An eGRID landing page on GitHub presenting power plant data."
+introContent: "Lorem ipsum dolor sit amet consectetur. In justo sit ullamcorper tempus nam enim adipiscing integer morbi. Id a ac a pellentesque a cras. Donec duis vestibulum pulvinar donec amet feugiat lectus urna gravida. Aliquet."
+sections:
+  - title: "eGRID Dev Portal"
+    sectionTitle: "eGRID Products"
+    sectionContent: "Repositories for released products"
+    cards:
+      - title: "eGRID Production Code"
+        content: "GRID methodology available for review and collaboration in the R programming language."
+        link: "https://github.com/usepa/egrid"
+        linkText: "Go to eGRID Production Code"
+        disabled: false
+      - title: "Power Profiler"
+        content: "Power Profiler code available in the R programming language."
+        link: "https://github.com/USEPA/power-profiler"
+        linkText: "Go to Power Profiler"
+        disabled: false
+      - title: "eGRID Explorer"
+        content: "Repository where eGRID Explorer code is available in the R programming language."
+        link: "https://github.com/USEPA/egrid-explorer"
+        linkText: "Go to eGRID Explorer"
+        disabled: false
+      - title: "PM<sub>2.5</sub>, NH<sub>3</sub>, and VOCs"
+        content: "Repository of R code used to determine PM<sub>2.5</sub>, NH<sub>3</sub>, and VOC emissions and emission rates."
+        link: "#"
+        linkText: "Coming Soon"
+        disabled: true
+    sectionTitle2: "eGRID Experiments"
+    sectionContent2: "Repositories for products in development"
+    cards2:
+      - title: "EPA-EIA Crosswalk"
+        content: "R code used to match EPA and EIA plants and units."
+        link: "https://github.com/USEPA/camd-eia-crosswalk"
+        linkText: "Go to Crosswalk"
+        disabled: false
+      - title: "Consumption-Based Rates"
+        content: "Methodology for consumption-based rates is available in Python."
+        link: "#"
+        linkText: "Coming Soon"
+        disabled: true
+      - title: "Monthly Data"
+        content: "R code is used to produce the monthly data for the annual eGRID release."
+        link: "#"
+        linkText: "Coming Soon"
+        disabled: true
+      - title: "EPA Hourly Fuel Type"
+        content: "R code used to estimate the hourly fuel type for units reporting data to EPA."
+        link: "#"
+        linkText: "Coming Soon"
+        disabled: true
+    sectionTitle3: "eGRID API"
+    sectionContent3: "Repositories for API"
+    cards3:
+      - title: "eGRID API"
+        content: "Repository to produce the eGRID API."
+        link: "#"
+        linkText: "Coming Soon"
+        disabled: true
+      - title: "Extract, Transfer, and Load"
+        content: "Process to format the eGRID R output data into the API format and populate the eGRID database."
+        link: "#"
+        linkText: "Coming Soon"
+        disabled: true
